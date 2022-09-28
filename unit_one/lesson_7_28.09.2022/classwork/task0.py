6cd6dc041fd0c035ac766685c08d764f536bae8c
vitaliy <chertkov.vitaliy@gmail.com>
2022-09-28 21:13:30 +0300

    Лекция 7

---
+++ unit_one/lesson_7_28.09.2022/classwork/task0.py
@@ -0,0 +1,16 @@
+# Написать программу, которая выдаёт на выходе точную копию своего исходного текста.
+file.xml
+XPATH
+DOM SAX
+<organization>
+<depart num=219>
+   <people>
+      <name>Петров </name>
+      <age>23 </age>
+   </people>
+<people>
+</depart>
+<depart num=21>
+
+</depart>
+</organization>
