9d08c39df68079fae21f5fff16ad9a3592f13bcc
vitaliy <chertkov.vitaliy@gmail.com>
2022-10-14 21:02:47 +0300

    Лекция 14 окт.

--- unit_one/lesson_13_12.10.2022/code/sea_battle.py
+++ unit_one/lesson_13_12.10.2022/code/sea_battle.py
@@ -4,7 +4,8 @@ LIVE = 1
 import sys
 
 def init():
-    matrix()
+    result = matrix()
+    print(result)
 
 def menu():
     out = f"1. Новая игра\n2. Продолжить игру\n3. Сохранить игру\n4.Загрузить игру"
@@ -16,21 +17,9 @@ def menu():
     else:
         sys.exit(0)
 
-def get_row(number_of_count = 5):
-    row = []
-    func = lambda: rdi(0, 1)
-    for i in range(number_of_count):
-        row.append(func)
-    result = []
-    for l in row:
-        result.append(l())
-    return result
-
-def matrix(amount_row = 5):
+def matrix(n = 5, m = 5):
     global game_field
-    for i in range(amount_row):
-        game_field.append(get_row())
-    return game_field
+    game_field = [[rdi(0, 1) for _ in range(n)] for _ in range(m)]
 
 def get_position():
     long = input("Введите координату x:")
@@ -54,26 +43,9 @@ def game():
     else:
         print("Ваша песенка спета!")
 
-
-
-
-#menu()
-keys = ["name", "age"]
-values = ["Peter", 12]
-lst = [{k: !} for k in keys for _ in values]
-print(lst)
-
-i_val
-str_name
-ft_pi
+menu()
 
 
 
 
-#
-# game_field.append(row_four)
-# game_field.append(row_five)
-# i = 1  # вхождение в первый массив
-# j = 4  # вхождение в 4-ый элемент текущего массива
-# # доступ к элементам двухмерного массива
-# print(game_field[i],[j])
+
