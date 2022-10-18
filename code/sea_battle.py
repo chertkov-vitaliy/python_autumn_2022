4156ea543f1b71fae4e1799ed273931731fa3b50
vitaliy <chertkov.vitaliy@gmail.com>
2022-10-18 11:50:11 +0300

    Лекция 15

--- code/sea_battle.py
+++ code/sea_battle.py
@@ -1,12 +1,32 @@
 from random import randint as rdi
-game_field = []
-LIVE = 1
 import sys
 
-def init():
-    result = matrix()
-    print(result)
+access = [1, 10, 100, 200, 300]
+game_field = []
+LIVE = 2
 
+def init():
+    """Инициируем начальное состояние игры!
+    Заполняем поле нулями.
+    """
+    matrix()
+
+def decorator_access(func_menu):
+    """
+    Декоратор для проверки доступа пользователя к игре
+    :param func_menu:
+    :return wrapper:
+    """
+    id = int(input("Введите ID"))
+    def wrapper():
+        global access
+        if id not in access:
+            sys.exit("Нет доступа! ")
+        else:
+            func_menu()
+    return wrapper
+
+@decorator_access
 def menu():
     out = f"1. Новая игра\n2. Продолжить игру\n3. Сохранить игру\n4.Загрузить игру"
     print(out)
@@ -32,20 +52,28 @@ def minus_live():
 
 def game():
     global game_field
-
     while LIVE:
         i, j = get_position()
         if game_field[i][j] == 1:
-            game_field[i][j]  = -1
+            game_field[i][j] = -1
             print("Попал")
+            render()
         else:
             minus_live()
     else:
         print("Ваша песенка спета!")
 
-menu()
+def render():
+    global game_field
+    str = ""
+    for row in game_field:
+        str = "\n"
+        for elm in row:
+            str += f" {elm} "
+        else:
+            print(str)
 
 
 
-
-
+if __name__ == "__main__":
+    menu()
