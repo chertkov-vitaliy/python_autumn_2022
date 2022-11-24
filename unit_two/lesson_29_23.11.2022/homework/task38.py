aa012b130cc443ebe6ee533b77e52cdfb9228131
vitaliy <chertkov.vitaliy@gmail.com>
2022-11-24 15:18:45 +0300

    Д.З. Л29

--- unit_two/lesson_29_23.11.2022/homework/task38.py
+++ unit_two/lesson_29_23.11.2022/homework/task38.py
@@ -1,5 +1,5 @@
 # todo: приложение game
-#  1. Рализовать в шаблоне игрового поля game.html вывод кол-ва неудачных попыток.
+#  1. Реализовать в шаблоне игрового поля game.html вывод кол-ва неудачных попыток.
 #  2. Реализовать роутер в app.py /users/list который отдает json со списком всех имеющийхся игроков (метод GET)
 #  3. Установить ассинхронную библиотеку https://docs.aiohttp.org/en/stable/
 #  получить через нее всех пользователей см. https://docs.aiohttp.org/en/stable/client_quickstart.html#json-response-content
